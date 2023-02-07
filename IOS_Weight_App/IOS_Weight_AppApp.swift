b66b3f3098c119db02d11c35b977004d10bd0d60
Erick <erk.rivs@gmail.com>
2023-02-07 15:33:50 -0500

    Initial Commit

---
+++ IOS_Weight_App/IOS_Weight_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Weight_AppApp.swift
+//  IOS_Weight_App
+//
+//  Created by Erick Rivera on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Weight_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
